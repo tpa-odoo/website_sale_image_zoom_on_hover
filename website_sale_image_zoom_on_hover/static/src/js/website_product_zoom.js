42765aeb3d705be2baa1c5336c5f6bb1de7dac43
Turkesh Patel <turkesh4friends@gmail.com>
2015-12-09 14:47:26 +0530

    IMProved code.

--- website_sale_image_zoom_on_hover/static/src/js/website_product_zoom.js
+++ website_sale_image_zoom_on_hover/static/src/js/website_product_zoom.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
 
     $('.oe_website_sale').each(function () {
         $('.image_zoom').zoom();
-        $('#ex2').zoom({ on:'grab' });
-        $('#ex3').zoom({ on:'click' });
-        $('#ex4').zoom({ on:'toggle' });
     });
 
 });
